689c9b20654289756e9d5eb7d9539c293cef95b3
Jeffry Gonzalez <jeff@hypertheory.com>
2020-06-24 11:34:25 -0400

    Add project files.

---
+++ BankingTests/BankAccountWithdrawals.cs
@@ -0,0 +1,39 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountWithdrawals
+    {
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(100)]
+        public void WithdrawingMoneyDecreasesBalance(decimal amountToWithdraw)
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            //var amountToWithdraw = 1M;
+
+            account.Withdraw(amountToWithdraw);
+
+            var expectedBalance = openingBalance - amountToWithdraw;
+            Assert.Equal(expectedBalance, account.GetBalance());
+        }
+
+
+       [Fact]
+       public void YouCanTakeAllYourMoney()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+
+            account.Withdraw(openingBalance);
+
+            Assert.Equal(0, account.GetBalance());
+        }
+    }
+}
---
+++ BankingTests/NewAccountTests.cs
@@ -0,0 +1,27 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+
+namespace BankingTests
+{
+    public class NewAccountTests
+    {
+        [Fact]
+        public void NewAccountsHaveCorrectBalance()
+        {
+            // "WTCYWYH" "Write the code you wish you had"
+            // Given I have a brand new bank account
+            var account = new BankAccount();
+            // When I retrive the balance
+            decimal balance = account.GetBalance();
+
+            // It has a blance of $5,000.00
+            Assert.Equal(5000M, balance);
+        }
+    }
+}
+
+// "Test Per Type" - BankAccount -> BankAccountTests, Customer -> CustomerTests
